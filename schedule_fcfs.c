50138c5e66b98b69a0c0105ba76ae2f2806db71d
ioseph01 <24601karma@gmail.com>
2025-11-19 14:37:20 -0800

    FCFS

--- schedule_fcfs.c
+++ schedule_fcfs.c
@@ -10,3 +10,33 @@
 #include "cpu.h"
 
 //Your code and design here
+
+
+ struct node *head = NULL;
+ struct node *tail = NULL;
+
+// add a task to the list 
+void add(char *name, int priority, int burst) {
+
+    struct task *newTask = malloc(sizeof(struct task));
+    newTask->name = name;
+    newTask->burst = burst;
+    if (head) {
+        insert(&(tail->next), newTask);
+        tail = tail->next; 
+    }
+    else {
+        insert(&head, newTask);
+        tail = head;
+    }
+
+
+}
+
+// invoke the scheduler
+void schedule() {
+    for (struct node *toRun = head; toRun; toRun = toRun->next) {
+        run(toRun->task, toRun->task->burst);
+        delete(&head, toRun->task);
+    }
+}
