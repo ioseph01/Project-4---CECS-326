ec6881d0c7847faa9fc0b0618a6db93f86120983
ioseph01 <24601karma@gmail.com>
2025-11-19 14:59:44 -0800

    round robin

--- schedule_rr.c
+++ schedule_rr.c
@@ -14,3 +14,42 @@
 /*
  * Your code and design here:
  */
+
+
+ struct node *head = NULL;
+ struct node *tail = NULL;
+
+// add a task to the list 
+void add(char *name, int priority, int burst) {
+
+    struct task *newTask = malloc(sizeof(struct task));
+    newTask->name = name;
+    newTask->burst = burst;
+    if (head) {
+        insert(&(tail->next), newTask);
+        tail = tail->next; 
+    }
+    else {
+        insert(&head, newTask);
+        tail = head;
+    }
+
+
+}
+
+// invoke the scheduler
+void schedule() {
+    // make it circular then run?
+    tail->next = head;
+    
+    for (struct node *toRun = head; toRun; toRun = toRun->next) {
+        run(toRun->task, QUANTUM);
+        toRun->task->burst -= QUANTUM;
+        if (toRun->task->burst <= 0) {
+            tail->next = NULL;
+            delete(&head, toRun->task);
+            tail->next = head;
+        }
+    }
+}
+
