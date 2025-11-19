df876a1d5bd0b27798bebabaf4f76701586cc4db
ioseph01 <24601karma@gmail.com>
2025-11-19 15:03:38 -0800

    priorty scheduling template

--- schedule_priority.c
+++ schedule_priority.c
@@ -12,3 +12,27 @@
 /*
  * Your code and design here:
  */
+
+ 
+
+ struct node *head = NULL;
+ struct node *tail = NULL;
+
+// add a task to the list 
+void add(char *name, int priority, int burst) {
+
+    struct task *newTask = malloc(sizeof(struct task));
+    newTask->name = name;
+    newTask->burst = burst;
+    // to whoever is going to work on this later, we can either sort it or insert it in order
+
+
+}
+
+// invoke the scheduler
+void schedule() {
+    for (struct node *toRun = head; toRun; toRun = toRun->next) {
+        run(toRun->task, toRun->task->burst);
+        delete(&head, toRun->task);
+    }
+}
