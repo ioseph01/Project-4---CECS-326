31180c1682b8ea1369fbb04ea141d0d6a2096f60
ioseph01 <24601karma@gmail.com>
2025-11-20 13:55:04 -0800

    priority

--- schedule_priority.c
+++ schedule_priority.c
@@ -23,14 +23,33 @@ void add(char *name, int priority, int burst) {
 
     struct task *newTask = malloc(sizeof(struct task));
     newTask->name = name;
+    newTask->priority = priority;
     newTask->burst = burst;
     // to whoever is going to work on this later, we can either sort it or insert it in order
-
-
+    if (head) {
+        insert(&(tail->next), newTask);
+        tail = tail->next; 
+    }
+    else {
+        insert(&head, newTask);
+        tail = head;
+    }
 }
 
 // invoke the scheduler
 void schedule() {
+
+    // bubble sort lol
+    for (struct node *it = head; it; it = it->next) {
+        for (struct node *other = it; other; other  = other->next) {
+            if (it->task->priority < other->task->priority) {
+                struct task *temp = it->task;
+                it->task = other->task;
+                other->task = temp;
+            }
+        }
+    }
+
     for (struct node *toRun = head; toRun; toRun = toRun->next) {
         run(toRun->task, toRun->task->burst);
         delete(&head, toRun->task);
